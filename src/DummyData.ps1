a2abadbf73bb9375f097aa6ec58e7729f5dd5e55
Heiko <61519853+htcfreek@users.noreply.github.com>
2023-07-16 12:29:57 +0200

    Merge version 1.2 into main (#11)

--- src/DummyData.ps1
+++ src/DummyData.ps1
@@ -1,18 +1,24 @@
-        $pwds = @([pscustomobject] @{
-            ComputerName = "test-pc"
-            Password = "MySmallTestPassword#"
-            PasswordUpdateTime = [datetime]::Now
-            ExpirationTimestamp = [datetime]::Now
-        })
-        $pwds += [pscustomobject] @{
-            ComputerName = "test-pc"
-            Password = "MyHistoryTestPasswordWithManyCharacters#"
-            PasswordUpdateTime = [datetime]::Parse("22.04.2023 10:40:56")
-            ExpirationTimestamp = $null
-        }
-        $pwds += [pscustomobject] @{
-            ComputerName = "test-pc"
-            Password = "MyHistoryTestPassword#"
-            PasswordUpdateTime = [datetime]::Parse("23.03.2023 10:10:23")
-            ExpirationTimestamp = $null
-        }
+    $pwds = @([pscustomobject] @{
+        ComputerName = "test-pc"
+        Password = "MySmallTestPassword#"
+        PasswordUpdateTime = [datetime]::Now
+        ExpirationTimestamp = [datetime]::Now
+    })
+    $pwds += [pscustomobject] @{
+        ComputerName = "test-pc"
+        Password = "MyHistoryTestPasswordWithManyCharacters#"
+        PasswordUpdateTime = [datetime]::Parse("22.04.2026 10:40:56")
+        ExpirationTimestamp = $null
+    }
+    $pwds += [pscustomobject] @{
+        ComputerName = "test-pc"
+        Password = "MyHistoryTestPassword#"
+        PasswordUpdateTime = [datetime]::Parse("23.03.2025 10:10:23")
+        ExpirationTimestamp = $null
+    }
+    $pwds += @([pscustomobject] @{
+        ComputerName = "test-pc"
+        Password = $null
+        PasswordUpdateTime = [datetime]::Parse("23.03.2000 10:10:23")
+        ExpirationTimestamp = $null
+    })
