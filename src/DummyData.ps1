a904620014d0eb77b743f1aa839d6b76ecc7008c
Heiko <61519853+htcfreek@users.noreply.github.com>
2025-12-07 16:27:44 +0100

    update dummy data

--- src/DummyData.ps1
+++ src/DummyData.ps1
@@ -1,23 +1,55 @@
-    $pwds = @([pscustomobject] @{
+$pwds = @([pscustomobject] @{
         ComputerName = "test-pc"
+        Account = "LapsAdmin"
         Password = "MySmallTestPassword#"
         PasswordUpdateTime = [datetime]::Now
         ExpirationTimestamp = [datetime]::Now
     })
     $pwds += [pscustomobject] @{
         ComputerName = "test-pc"
+        Account = "LapsAdmin"
         Password = "MyHistoryTestPasswordWithManyCharacters#"
         PasswordUpdateTime = [datetime]::Parse("22.04.2026 10:40:56")
         ExpirationTimestamp = $null
     }
     $pwds += [pscustomobject] @{
         ComputerName = "test-pc"
+        Account = "LapsAdmin"
         Password = "MyHistoryTestPassword#"
+        PasswordUpdateTime = [datetime]::Parse("23.03.2025 10:10:23")
+        ExpirationTimestamp = $null
+    }
+        $pwds += [pscustomobject] @{
+        ComputerName = "test-pc"
+        Account = "LapsAdmin"
+        Password = "MyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPassword#"
+        PasswordUpdateTime = [datetime]::Parse("23.03.2025 10:10:23")
+        ExpirationTimestamp = $null
+    }
+            $pwds += [pscustomobject] @{
+        ComputerName = "test-pc"
+        Account = "LapsAdmin"
+        Password = "MyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPassword#"
+        PasswordUpdateTime = [datetime]::Parse("23.03.2025 10:10:23")
+        ExpirationTimestamp = $null
+    }
+            $pwds += [pscustomobject] @{
+        ComputerName = "test-pc"
+        Account = "LapsAdmin"
+        Password = "MyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPassword#"
+        PasswordUpdateTime = [datetime]::Parse("23.03.2025 10:10:23")
+        ExpirationTimestamp = $null
+    }
+            $pwds += [pscustomobject] @{
+        ComputerName = "test-pc"
+        Account = "LapsAdmin"
+        Password = "MyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPasswordMyHistoryTestPassword#"
         PasswordUpdateTime = [datetime]::Parse("23.03.2025 10:10:23")
         ExpirationTimestamp = $null
     }
     $pwds += @([pscustomobject] @{
         ComputerName = "test-pc"
+        Account = $null
         Password = $null
         PasswordUpdateTime = [datetime]::Parse("23.03.2000 10:10:23")
         ExpirationTimestamp = $null
